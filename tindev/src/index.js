7cc570e3c2d19048babf9f3b52a6fa008d1a5d29
gabriel-severo <gabriel.ivi@hotmail.com>
2020-03-25 13:26:53 -0300

    Add match function in mobile

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Routes from './routes'
+import {YellowBox} from 'react-native';
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+])
 
 export default function App() {
   return <Routes/>
